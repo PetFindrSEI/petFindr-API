07e9773f0ada9f17fd72a4b61b0ae4afabd71a7f
spell-landon <spell.landon@gmail.com>
2022-02-12 11:18:16 -0600

    Complete Model Co-authored-by: Melissa Morgan melissamorganc@gmail.com

---
+++ petfindr/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+class Pet(models.Model):
+    STATUS_CHOICES = [
+        ('Lost', 'Lost'),
+        ('Found', 'Found'),
+    ]
+    TYPE_CHOICES = [
+        ('Dog', 'Dog'),
+        ('Cat', 'Cat'),
+        ('Other', 'Other'),
+    ]
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('Uk', 'Unknown'),
+    ]
+    SIZE_CHOICES = [
+        ('XS', 'Extra Small'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+    ]
+    MICROCHIP_CHOICES = [
+        ('Y', 'Yes'),
+        ('N', 'No'),
+        ('Uk', 'Unknown'),
+    ]
+
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    name = models.CharField(max_length=100, default='Unknown')
+    type = models.CharField(max_length=100, choices=TYPE_CHOICES, default='Dog')
+    gender = models.CharField(max_length=100, choices=GENDER_CHOICES)
+    size = models.CharField(max_length=100, choices=SIZE_CHOICES)
+    color = models.CharField(max_length=100)
+    description = models.TextField()
+    microchip = models.CharField(max_length=100, choices=MICROCHIP_CHOICES, default='Unknown')
+    location = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    reported_time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.name} - {self.status} - {self.type}'
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE petfindr;
+CREATE USER petfindruser WITH PASSWORD 'petfindr';
+GRANT ALL PRIVILEGES ON DATABASE petfindr TO petfindruser;
