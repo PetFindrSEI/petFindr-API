b1d8101dd5cdda1f8138625ff77e4d10f605b15f
spell-landon <spell.landon@gmail.com>
2022-02-12 12:57:09 -0600

    Complete Backend minus AWS and Deployment, Create User Model Co-authored-by: Melissa Morgan melissamorganc@gmail.com

--- petfindr_django/urls.py
+++ petfindr_django/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('petfindr.urls')),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
-]
+    # path('api/', include('petfindr.urls')),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('users.urls')),
+    path('', include('petfindr.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# tell our application where to look for media files
